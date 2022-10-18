5ea633a6048fcc613fd10c91d563fdea44c758ac
Fabiano Camargo <contadofabiano@gmail.comm>
2022-10-18 15:38:46 -0300

    before implementing database

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,25 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+import globalStyleUrl from "./styles/global.css";
+import globalMediumStyleUrl from "./styles/global-medium.css";
+import globalLargeStyleUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: globalStyleUrl },
+    { rel: "stylesheet", href: globalMediumStyleUrl, media: "print, (min-width: 640px)" },
+    { rel: "stylesheet", href: globalLargeStyleUrl, media: "screen and (min-width: 1024px)" },
+  ];
+};
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Remix: So greate, it's funny""</title>
+        <title>Remix: So greate, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,6 @@
 import type { LinksFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+
 import stylesUrl from '~/styles/index.css';
 
 export const links: LinksFunction = () => {
@@ -9,9 +11,19 @@ export const links: LinksFunction = () => {
 
 export default function IndexRoute() {
   return (
-    <div>
-      <h1>Index Route</h1>
-      <p>Here is the index route</p>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
